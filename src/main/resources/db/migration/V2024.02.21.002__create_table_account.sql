5c059fad5aa83464042c1e7161d131019fe68e31
Sergio Eduardo Ramella Junior <101378895+st4pzz@users.noreply.github.com>
2024-04-10 10:39:27 -0300

    Update V2024.02.21.002__create_table_account.sql

--- src/main/resources/db/migration/V2024.02.21.002__create_table_account.sql
+++ src/main/resources/db/migration/V2024.02.21.002__create_table_account.sql
@@ -1,8 +1,8 @@
-CREATE TABLE inscricao
+CREATE TABLE partida
 (
-    id_inscricao character varying(36) NOT NULL,
-    tx_id_user character varying(36) NOT NULL,
-    tx_id_job character varying(36) NOT NULL,
-    tx_status character varying(36) NOT NULL,
+    id_partida character varying(36) NOT NULL,
+    time1 character varying(36) NOT NULL,
+    time2 character varying(36) NOT NULL,
+    id_jogador character varying(36) NOT NULL,
     CONSTRAINT inscricao_pkey PRIMARY KEY (id_inscricao)
 );
