05bc2604b9f6f208643b5f6d7bb011e440055159
st4pzz <ramellajr@gmail.com>
2024-04-10 17:57:21 -0300

    update partida resource

--- src/main/resources/db/migration/V2024.02.21.002__create_table_account.sql
+++ src/main/resources/db/migration/V2024.02.21.002__create_table_account.sql
@@ -4,5 +4,5 @@ CREATE TABLE partida
     time1 character varying(36) NOT NULL,
     time2 character varying(36) NOT NULL,
     id_jogador character varying(36) NOT NULL,
-    CONSTRAINT inscricao_pkey PRIMARY KEY (id_inscricao)
+    CONSTRAINT partida_pkey PRIMARY KEY (id_partida)
 );
