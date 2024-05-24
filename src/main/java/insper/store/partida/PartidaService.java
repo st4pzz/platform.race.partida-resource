60b4db219f863b236e197a0acc4036dda334f12b
st4pzz <ramellajr@gmail.com>
2024-05-23 22:49:03 -0300

    redis

--- src/main/java/insper/store/partida/PartidaService.java
+++ src/main/java/insper/store/partida/PartidaService.java
@@ -3,6 +3,9 @@ package insper.store.partida;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import lombok.NonNull;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CachePut;
+
 @Service
 public class PartidaService {
 
@@ -11,15 +14,17 @@ public class PartidaService {
 
     
     
-
+    @CachePut(value = "partidas", key = "#result.id")
     public Iterable<PartidaModel> readAll() {
         return PartidaRepository.findAll();
     }
 
+    @CachePut(value = "partidas", key = "#result.id")
     public Partida create(Partida in) {
         return PartidaRepository.save(new PartidaModel(in)).to();
     }
 
+    @Cacheable(value = "partidas", key = "#id")
     public Partida read(@NonNull String id) {
         return PartidaRepository.findById(id).map(PartidaModel::to).orElse(null);
     }
